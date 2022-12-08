79d9cbf96956763bc0e962e1e02e5ce20b45dd61
Nijji <alwinsenior@outlook.com>
2022-12-08 11:30:30 +0300

    updated protected route

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
-const express = require('express')
-const jwt=require('jsonwebtoken')
-const app = express()
-
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
 
 //app.use('/api',require('./routes/api'))
-app.post('/api/post',(req,res) =>
-{
-    res.json({ message: 'my json api' })
-    
-})
-app.post("/api/login", (req, res) => {
-    const user = {
-        id: 1,
-        name: 'nijji',
-        email:'nijji@gmail.com'
+app.post("/api/posts", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({ message: "my json api created" }), authData;
     }
-    jwt.sign({ user }, 'secret key', (err,token) =>
-    {
-      res.json({token})  
-    })
+  });
+});
+app.post("/api/login", (req, res) => {
+  const user = {
+    id: 1,
+    name: "nijji",
+    email: "nijji@gmail.com",
+  };
+  jwt.sign({ user }, "secret key", (err, token) => {
+    res.json({ token });
+  });
 });
 
-app.listen(5000)
+//verifyToken
+function verifyToken(req, res, next) {
+  //auth header value
+  const bearerHeader = req.headers["authorization"];
+  //if undefined
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    //forbidden
+    res.sendStatus(403, (err) =>
+    {
+      err.msg
+    });
+  }
+}
+
+app.listen(5000);
